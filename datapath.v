455812856ca64ae8352a3e71c8b7f62ff9c1e75c
Connor Johnston <20cjcj@queensu.ca>
2024-02-15 11:26:40 -0500

    final commit lab 4

--- datapath.v
+++ datapath.v
@@ -1,13 +1,13 @@
 module datapath(
 	input wire clock, clear,
 	input wire [31:0] Mdatain, Z,
-	
 	input wire HIin, LOin, PCin, Zin, Yin, IRin, MARin, MDRin, Read, IncPC, Csignextendedin, R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in
 );
 wire HIout, LOout, PCout, Zhighout, Zlowout, MDRout;
 wire[15:0] rOut;
 wire [31:0] BusMuxOut, BusMuxInCsignextended, BusMuxInHI, BusMuxInLO, BusMuxInZ, BusMuxInZlow, BusMuxInZhigh, BusMuxInPC, BusMuxInMDR, BusMuxInMAR, BusMuxInMdata, BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, BusMuxInR7, BusMuxInR8, BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12, BusMuxInR13, BusMuxInR14, BusMuxInR15;
 wire [4:0] BusMuxSelect;
+wire [31:0] MuxToMDR;
 edgetrigreg R0(clear, clock, R0in, BusMuxOut, BusMuxInR0);
 edgetrigreg R1(clear, clock, R1in, BusMuxOut, BusMuxInR1);
 edgetrigreg R2(clear, clock, R2in, BusMuxOut, BusMuxInR2);
@@ -34,6 +34,8 @@ edgetrigreg MDR(clear, clock, MDRin, BusMuxOut, BusMuxInMDR);
 edgetrigreg MAR(clear, clock, MARin, BusMuxOut, BusMuxInMAR);
 edgetrigreg Mdata(clear, clock, Mdatain, BusMuxOut, BusMuxInMdata);
 edgetrigreg Csignextended(clear, clock, Csignextendedin, BusMuxOut, BusMuxInCsignextended);
+
+MDMux mdm(BusMuxOut, Mdatain, Read, MUXtoMDR)
 
 bidirectional_bus bus(BusMuxSelect, BusMuxInR0,BusMuxInR1,BusMuxInR2,BusMuxInR3,BusMuxInR4,BusMuxInR5,BusMuxInR6,BusMuxInR7,BusMuxInR8,BusMuxInR9,BusMuxInR10,BusMuxInR11,BusMuxInR12,BusMuxInR13,BusMuxInR14,BusMuxInR15,BusMuxInHI,BusMuxInLO,BusMuxInZlow,BusMuxInZhigh,BusMuxInPC,BusMuxInMDR, BusMuxInCsignextended, BusMuxOut);
 BusMuxEncoder bme({HIout, LOout, PCout, Zhighout, Zlowout, MDRout, rOut}, BusMuxSelect);
