4def04ced700abeae634c3f31435cb4338b8057e
Ajollymore <ajjollymore@gmail.com>
2024-03-05 17:35:48 -0500

    suck my dick cho

--- and_tb.v
+++ and_tb.v
@@ -5,16 +5,19 @@ module and_tb;
  reg IncPC, Read, AND, R1in, R2in, R3in;
  reg Clock;
  reg [31:0] Mdatain;
+ reg clear;
 parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
  Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
  T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
  reg [3:0] Present_state = Default;
-datapath DUT(PCout, Zlowout, MDRout, R2out, R3out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, AND, R1in,
-R2in, R3in, Clock, Mdatain);
+datapath DUT(.PCout(PCout), .Zlowout(Zlowout), .MDRout(MDRout), .R2out(R2out), .R3out(R3out), .MARin(MARin),
+.Zin(Zin), .PCin(PCin), .MDRin(MDRin), .IRin(IRin), .Yin(Yin), .IncPC(IncPC), .Read(Read),
+.R1in(R1in), .R2in(R2in), .R3in(R3in), .clock(Clock), .Mdatain(Mdatain), .clear(clear));
 // add test logic here
 initial
  begin
  Clock = 0;
+ clear = 0;
  forever #10 Clock = ~ Clock;
 end
 always @(posedge Clock) // finite state machine; if clock rising-edge
@@ -50,19 +53,19 @@ Reg_load1a: begin
 Mdatain <= 32'h00000012;
 Read = 0; MDRin = 0; // the first zero is there for completeness
 #10 Read <= 1; MDRin <= 1; // and the first 10ns might not be needed depending on your
-#15 Read <= 0; MDRin <= 0; // implementation; same goes for the other states
+ #15 Read <= 0; MDRin <= 0; 
 end
  Reg_load1b: begin
- #10 MDRout <= 1; R2in <= 1;
+ #10    MDRout <= 1; R2in <= 1;
  #15 MDRout <= 0; R2in <= 0; // initialize R2 with the value $12
 end
 Reg_load2a: begin
 Mdatain <= 32'h00000014;
-#10 Read <= 1; MDRin <= 1;
+#10    Read <= 1; MDRin <= 1;
 #15 Read <= 0; MDRin <= 0;
 end
  Reg_load2b: begin
- #10 MDRout <= 1; R3in <= 1;
+ #10    MDRout <= 1; R3in <= 1;
  #15 MDRout <= 0; R3in <= 0; // initialize R3 with the value $14
 end
 Reg_load3a: begin
@@ -75,22 +78,27 @@ end
  #15 MDRout <= 0; R1in <= 0; // initialize R1 with the value $18
 end
 T0: begin // see if you need to de-assert these signals
-PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1;
+ PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1;
 end
 T1: begin
+ PCout <= 0; MARin <= 0; IncPC <= 0; Zin <= 0;
 Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1;
 Mdatain <= 32'h28918000; // opcode for “and R1, R2, R3”
 end
 T2: begin
+Zlowout <= 0; PCin <= 0; Read <= 0; MDRin <= 0;
 MDRout <= 1; IRin <= 1;
 end
 T3: begin
+MDRout <= 0; IRin <= 0;
 R2out <= 1; Yin <= 1;
 end
 T4: begin
+R2out <= 0; Yin <= 0;
 R3out <= 1; AND <= 1; Zin <= 0; // de-assert Zin
 end
 T5: begin
+R3out <= 0; AND <= 0; 
 Zlowout <= 0; R1in <= 0; // de-assert Zlowout and R1in
 end
  endcase
