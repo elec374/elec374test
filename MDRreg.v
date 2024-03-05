94db5e47e98030809aeeb51ac3c8c0b9c1a560b5
Siyung Cho <20fsc@queensu.ca>
2024-03-05 18:59:51 -0500

    work on tb

--- MDRreg.v
+++ MDRreg.v
@@ -1,13 +1,10 @@
-module MDRreg(
-	input [31:0] D,
-	input clk,clr,enable,
-	output [31:0] Q,
-	reg[31:0] Q,
-);
-
-always @(posedge clk)
-	begin
-		if(clr)
-			Q <= 0;
-		else if(enable)
-			Q <= D;
+module MDRreg (clr, clk, enable, Mdatain, BusMuxOut, read, MDRout);
+    input clr, clk, enable, read;
+    input [31:0] Mdatain, BusMuxOut;
+    output  wire [31:0] MDRout;
+	
+    wire [31:0] MDRin;
+    mux2x1 MDMux (Mdatain, BusMuxOut, read, MDRin);
+    edgetrigreg RegMDR (clr, clk, enable, MDRin, MDRout);
+		
+endmodule 
--- bidirectional_bus.v
+++ bidirectional_bus.v
@@ -1,45 +1,47 @@
 module bidirectional_bus (
-	//Mux
-	input wire[4:0] BusMuxSelect,
-	//Encoder
-	input [31:0] BusMuxInR0, BusMuxInR1,  BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, 
-	BusMuxInR7, BusMuxInR8,BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12,BusMuxInR13, BusMuxInR14,BusMuxInR15,
-	BusMuxInHI,BusMuxInLO, BusMuxInZlow, BusMuxInZhigh,BusMuxInPc, BusMuxInMDR, BusMuxInCsignextended, BusMuxInIR, BusMuxInCout,
+	input wire[5:0] BusMuxSelect,
 
-	output wire [31:0]BusMuxOut
+	input [31:0] BusMuxInR0, BusMuxInR1,  BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, BusMuxInR7, BusMuxInR8,BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12,BusMuxInR13, BusMuxInR14,BusMuxInR15,
+	
+	input [31:0] BusMuxInHI, BusMuxInLO, BusMuxInZHI, BusMuxInZLO, BusMuxInPC, BusMuxInMDR, BusMuxInPort, BusMuxInCsignextended,
+
+	output wire [31:0] BusMuxOut
 );
 
-reg [31:0]q;
+reg [31:0] q;
 
 always @ (*) begin
 	case(BusMuxSelect)
-		5'd0	:	q <= BusMuxInR0;
-		5'd1	:	q <= BusMuxInR1;
-		5'd2	:	q <= BusMuxInR2;
-		5'd3	:	q <= BusMuxInR3;
-		5'd4	:	q <= BusMuxInR4;
-		5'd5	:	q <= BusMuxInR5;
-		5'd6	:	q <= BusMuxInR6;
-		5'd7	:	q <= BusMuxInR7;
-		5'd8	:	q <= BusMuxInR8;
-		5'd9	:	q <= BusMuxInR9;
-		5'd10	:	q <= BusMuxInR10;
-		5'd11	:	q <= BusMuxInR11;
-		5'd12	:	q <= BusMuxInR12;
-		5'd13	:	q <= BusMuxInR13;
-		5'd14	:	q <= BusMuxInR14;
-		5'd15	:	q <= BusMuxInR15;
-		5'd16	:	q <= BusMuxInHI;
-		5'd17	:	q <= BusMuxInLO;
-		5'd18	:	q <= BusMuxInZlow;
-		5'd19	:	q <= BusMuxInZhigh;
-		5'd20	:	q <= BusMuxInPc;
-		5'd21	:	q <= BusMuxInMDR;
-		5'd23	:	q <= BusMuxInCsignextended;
-		5'd24	: 	q <= BusMuxInIR;		
-		5'd32 : 	q <= 32'hF;
-		default	:	q <= 32'd0;
-		endcase
+		5'd0	:	q <= BusMuxInR0[31:0];
+		5'd1	:	q <= BusMuxInR1[31:0];
+		5'd2	:	q <= BusMuxInR2[31:0];
+		5'd3	:	q <= BusMuxInR3[31:0];
+		5'd4	:	q <= BusMuxInR4[31:0];
+		5'd5	:	q <= BusMuxInR5[31:0];
+		5'd6	:	q <= BusMuxInR6[31:0];
+		5'd7	:	q <= BusMuxInR7[31:0];
+		5'd8	:	q <= BusMuxInR8[31:0];
+		5'd9	:	q <= BusMuxInR9[31:0];
+		5'd10	:	q <= BusMuxInR10[31:0];
+		5'd11	:	q <= BusMuxInR11[31:0];
+		5'd12	:	q <= BusMuxInR12[31:0];
+		5'd13	:	q <= BusMuxInR13[31:0];
+		5'd14	:	q <= BusMuxInR14[31:0];
+		5'd15	:	q <= BusMuxInR15[31:0];
+		
+		5'd16	:	q <= BusMuxInHI[31:0];
+		5'd17	:	q <= BusMuxInLO[31:0];
+		5'd18	:	q <= BusMuxInZHI[31:0];
+		5'd19	:	q <= BusMuxInZLO[31:0];
+		5'd20	:	q <= BusMuxInPC[31:0];
+		5'd21	:	q <= BusMuxInMDR[31:0];
+		5'd22	:	q <= BusMuxInPort[31:0];
+		5'd23	: 	q <= BusMuxInCsignextended[31:0];
+		
+		default: q = 32'd0;
+	endcase
 end
+
 assign BusMuxOut = q;
+
 endmodule 
